c8b43214c5305c14b8fc5eac2629149e7615b344
PRICIDON <dddbjjk6@gmail.com>
2025-08-27 22:56:06 +0300

    03: clerk auth

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -3,14 +3,25 @@ import { mutation, query } from "./_generated/server";
 
 export const getMany = query({
   args: {},
-  handler: async (ctx) => {
+  handler: async (ctx: {
+    db: {
+      query: (arg0: string) => {
+        (): any;
+        new (): any;
+        collect: { (): any; new (): any };
+      };
+    };
+  }) => {
     return await ctx.db.query("users").collect();
   },
 });
 
 export const add = mutation({
   args: {},
-  handler: async (ctx) => {
+  handler: async (ctx: {
+    auth: { getUserIdentity: () => any };
+    db: { insert: (arg0: string, arg1: { name: string }) => any };
+  }) => {
     const identity = await ctx.auth.getUserIdentity();
     if (identity === null) {
       throw new Error("Not authenticated");
